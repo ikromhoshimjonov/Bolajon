42eaccfef40e933c0f7fcc3e538d1de8da459cd1
Ikromjon Hoshimjonov <hoshimjonovikromjon4@gmail.com>
2026-07-18 19:48:27 +0500

    fix: static files white-noise and csrf fixes

--- root/settings.py
+++ root/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-xddx2bce@uncqh6%3-^b5^r_j0gun)96wnc$x$jseev-3ouc$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['bolajon-ewmo.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
